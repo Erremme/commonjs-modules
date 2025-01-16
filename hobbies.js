e02390a91b9b08c70900d8d42bf53a79e8ef1be6
Erremme <Erremme2.0@gmail.com>
2025-01-16 14:46:15 +0100

    feat : create a function and export

--- hobbies.js
+++ hobbies.js
@@ -1,2 +1,14 @@
 //1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
 //2. Esporta la funzione dal file.
+
+function createHobbies(hobbyOne, hobbyTwo, hobbyThree){
+    return [hobbyOne = "Football", 
+            hobbyTwo = "Fishing", 
+            hobbyThree = " Watching Movies"];
+
+  }
+
+  module.export = createHobbies;
+
+  const hobby = createHobbies()
+  console.log(hobby)
