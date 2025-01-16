0c20e035080d585e612b0372fa52ad2d90ebcf94
Erremme <Erremme2.0@gmail.com>
2025-01-16 14:36:21 +0100

    feat : create  a function and export

--- names.js
+++ names.js
@@ -1,2 +1,12 @@
 //1.Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 //2. Esporta la funzione dal file.
+
+
+function createNames( firstName , lastName){
+  return firstName = "Raffaele " , 
+          lastName = "Molinaro";
+}
+
+
+
+module.exports = createNames;
